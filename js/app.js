f182bbfbae0578d9f8b04921671eb9ea683651b3
Jacob Moller <jacob.moller8@hotmail.com>
2019-01-26 21:04:11 +0100

    added controllers in app.js

--- js/app.js
+++ js/app.js
@@ -6,13 +6,19 @@ $(function () {
 	var exampleView = new ExampleView($("#exampleView"));
 
 	var DinnerOverviewView = new dinnerOverviewView($('#dinner_overview'), model);
+	var DinnerOverviewController = new dinnerOverviewController($(DinnerOverviewView), model);
+
 	var SelectDishView = new selectDishView($('#select_dish'), model);
+	var SelectDishController = new selectDishController($(SelectDishView), model);
+
 	var DinnerPrintoutView = new dinnerPrintoutView($('#dinner_printout'), model);
+	var DinnerPrintoutController = new dinnerPrintoutController($(DinnerPrintoutView), model);
+
 	var DishDetailsView = new dishDetailsView($('#dish_details'), model);
+	var DishDetailsController = new dishDetailsController($(DishDetailsView), model);
+
 	var SelectDishAgainView = new selectDishAgainView($('#select_dish_view'), model);
-	
-
-
+	var SelectDishAgainController = new selectDishAgainController($(SelectDishAgainView), model);
 
 
 	/**
