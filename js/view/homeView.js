2b2be37ded64066d1ec4b4f39e29ae8b1017eb95
Hugo B <hugo@bergqvist.com>
2019-01-27 17:28:20 +0100

    removed generalController.js

--- js/view/homeView.js
+++ js/view/homeView.js
@@ -1,10 +1,3 @@
 var homeView = function (container, model) {
-	console.log("reached");
-	var newDinnerBtn = container.getElementById("#newDinnerButton");
-	var listener = function (evt) {
-		alert(evt.type + ' event on "' + evt.target.innerHTML + '"');
-	}
-
-	newDinnerBtn.addEventListener("click", listener, false);
 
 }
