2ce5e1b79d51c60dc4242cb75db38d9bbe157ae2
Jacob Moller <jacob.moller8@hotmail.com>
2019-01-29 10:37:59 +0100

    fix error bug

--- js/controller/dishDetailsController.js
+++ js/controller/dishDetailsController.js
@@ -3,9 +3,13 @@ var dishDetailsController = function (view, model, app) {
 
 	view.onload = console.log("view file loaded, här hade jag velat lägga till knappfunktionen");
 	//view.backToSearchBtn.click(clickEvt);
-	
 
-	var clickEvt = function(){
+
+	var clickEvt = function () {
+		view.container.find(".dishBtn").click(function () {
+			model.setSelectedDishId(this.id);
+			app.showDishDetailsScreen();
+		})
 		console.log("clicked!");
 		app.showSelectDishScreen();
 	}
--- js/controller/dishSearchController.js
+++ js/controller/dishSearchController.js
@@ -4,17 +4,16 @@ var dishSearchController = function (view, model, app) {
         view.searchUpdate();
     });
 
-    this.updateDishes = function () {
+    this.update = function () {
         ImageClick();
-		}
-		
+    }
+
     var ImageClick = function () {
         view.container.find(".dishBtn").click(function () {
+            console.log("hej")
             model.setSelectedDishId(this.id);
             app.showDishDetailsScreen();
         })
     }
     ImageClick();
-
-
 }
